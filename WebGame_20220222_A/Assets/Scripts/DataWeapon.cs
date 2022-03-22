4421105fc1253698b1a361684259d2e355796366
JaseOwO <100176030+JaseOwO@users.noreply.github.com>
2022-03-22 12:02:18 +0800

    完成角色控制器及添加武器資料

---
+++ WebGame_20220222_A/Assets/Scripts/DataWeapon.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace JASE
+{
+    /// <summary>
+    /// 武器資料
+    /// 1. 飛行速度
+    /// 2. 攻擊力
+    /// 3. 數量
+    /// 4. 數量上限
+    /// 5. 生成位置
+    /// </summary>
+    [CreateAssetMenu(menuName = "JASE/Date Weapon", fileName = "Data Weapon")]
+    public class DataWeapon : ScriptableObject
+    {
+        
+    }
+}
